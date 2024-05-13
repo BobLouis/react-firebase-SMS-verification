8d63f328dc88a8c25ba97c9ddc6d7913a9207e91
Louis <bob900316@gmail.com>
2024-05-13 19:50:20 +0800

    init

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCckUFIHuQR3KbqmcXsTWmgB8_HcDNbOGA",
+  authDomain: "authentication-5c27d.firebaseapp.com",
+  projectId: "authentication-5c27d",
+  storageBucket: "authentication-5c27d.appspot.com",
+  messagingSenderId: "343642812185",
+  appId: "1:343642812185:web:e118db942e6064abf49f88"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
